fb39a6aa34a09dcf9f378bd8e2f4836cf4bc6fea
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-04-03 10:02:44 +0200

    Renaming

--- src/main/java/de/workshops/bookshelf/config/OpenApiConfiguration.java
+++ src/main/java/de/workshops/bookshelf/config/OpenApiConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfiguration {
 
     @Bean
     @Profile("dev")
